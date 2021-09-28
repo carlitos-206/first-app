7408cb2285ff72147036d5671ec419b642594045
Carlos Caceres <carloscaceres041@gmail.com>
2021-09-28 00:34:30 -0700

    lenovo back up

--- first-app.py
+++ first-app.py
@@ -1,7 +1,9 @@
 import math
+import sys 
+
 print("\nWELCOME TO THE CALCULATOR APP\n")
 print("\nTO START ENTER AN OPTION VALUE OR 9 FOR MENU OPTIONS\n") 
-class app: 
+class app:
     sum = 0 
     def __init__(self, val):
         self.val = val
@@ -12,33 +14,53 @@ class app:
                 global sum 
                 sum = this_sum
                 print(f"\n = {sum}\n")
-                return app(input("ENTER NEW MENU OPTION OR 9 for HELP: ")) 
+                return app(input("ENTER 2 TO ADD MORE OR ENTER A NEW MENU OPTION OR 9 FOR HELP: ")) 
             print(add(a=int(input("\nFIRST VALUE: ")), b=int(input("\nSECOND VALUE: "))))
         if self.val=="2":
             global sum
             if sum == 0:
-                return add(a=int(input("\nFIRST VALUE: ")), b=int(input("\nSECOND VALUE: ")))
+                def add(a,b):
+                    this_sum=a+b
+                    global sum
+                    sum = this_sum
+                    print(f"\n = {sum}\n")
+                    return app(input("\nENTER 2 TO ADD MORE OR ENTER A NEW MENU OPTION OR 9 FOR HELP: "))
+                print("\nVALUES ARE NEEDED", add(a=int(input("\nFIRST VALUE: ")), b=int(input("\nSECOND VALUE: "))))
             else:
-                print("ADD ANOTHER VALUE TO THE SUM")
+                print("\nADD ANOTHER VALUE TO THE SUM\n")
                 def addMore(n):
                     global sum
-                    sum = sum + n
-                    print(sum)
-                    return app(input("ENTER NEW MENU OPTION OR 9 for HELP: "))
+                    this_sum = sum +n
+                    sum = this_sum
+                    print(f"\n = {sum}")
+                    return app(input("\nENTER NEW MENU OPTION OR 9 FOR HELP: "))
             print(addMore(n=int(input(""))))
+        if self.val == "3":
+            print("\nWE ARE CLEARING THE GLOBAL SUM...\n")
+            if sum > 0:
+                sum-=sum
+                print (f"\nTHE GLOBAL SUM IS NOW {sum}\n")
+            elif sum == 0:
+                print("THE SUM IS ALREADY 0")
+            return app(input("\nENTER NEW MENU OPTION OR 9 FOR HELP; "))
         if self.val=="9":
-            print("""THESE ARE THE OPTION THAT ARE MEANT FOR THE MENU:
+            
+            print(f"\nTHIS IS THE CURRENT SUM VALUE {sum}")
+            print("""\nTHESE ARE THE OPTION THAT ARE MEANT FOR THE MENU:
             1 - ADDING TWO VALUES
+            2 - ADD TO CURRENT SUM VALUE
+            3 - CLEAR THE SUM 
             9 - HELP
             0 - EXIT""")
             return app(input("ENTER MENU OPTION: "))
         if self.val=="0":
-            print("\nGOODBYE\n")
-            return None
-        #if self.val!=int:
-         #   print("\nTHE VALUE MUST BE A NUMBER, FOR HELP 9")
-          #  return app(input("ENTER MENU OPTION: "))
+            
+            #This is how the app closes with sys.exit() function 
+            
+            sys.exit("\nTHANK YOU, GOODBYE\n")
         else:
             print("\nENTER A PROPER OPTION OR 9 FOR MENU OPTIONS\n")
-            return app(input("ENTER MENU OPTION: "))
+            return app(input("\nENTER MENU OPTION: "))
 user=app(val=input())
+
+#this a tes
